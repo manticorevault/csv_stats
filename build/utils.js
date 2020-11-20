58910ed926a59c1f01ffba00cc1296cbbbb26854
manticorevault <artur_seilah@hotmail.com>
2020-11-20 17:54:48 +0000

    Created the Utils file to iterate over the datestring and convert it to an actual Date in a cleaner way.

---
+++ build/utils.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.dateStringToData = void 0;
+exports.dateStringToData = function (dateString) {
+    var dateParts = dateString.split('/').map(function (value) {
+        return parseInt(value);
+    });
+    return new Date(dateParts[2], dateParts[1] - 1, dateParts[0]);
+};
