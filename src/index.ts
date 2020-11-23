acf3b7eaa8dc29b43123fab101cca694f2e4c9da
manticorevault <artur_seilah@hotmail.com>
2020-11-23 19:27:34 +0000

    Finished the analyzer and refactored it to make the code cleaner

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,8 @@
 import { MatchReader } from './MatchReader'
-import { CsvFileReader } from './CsvFileReader'
-import { MatchResult } from './MatchResult'
+import { Summary } from './Summary'
 
-// An Object that satisfies the "DataReader interface"
-const csvFileReader = new CsvFileReader('football.csv')
+const matchReader = MatchReader.fromCsv('football.csv')
+const summary = Summary.winsAnalysis('Man United')
 
-// Create an instance of MatchReader and pass something satisfying
-const matchReader = new MatchReader(csvFileReader)
 matchReader.load()
-
-// Access matchReader.matches
-
-let manUnitedWins = 0
-
-for (let match of matchReader.matches) {
-  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
-    manUnitedWins++
-  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
-    manUnitedWins++
-  }
-}
-
-console.log(`Man United won ${manUnitedWins} games`)
+summary.buildAndPrintReport(matchReader.matches)
